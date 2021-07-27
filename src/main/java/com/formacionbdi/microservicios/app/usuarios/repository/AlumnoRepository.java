e2117d9b0b5d01a11607415b35f8337fd5318d06
jorge.cruz <jorge.cruz@elektra.com.mx>
2021-07-27 18:49:04 -0500

    Se agrega metodo para buscar por alumno

--- src/main/java/com/formacionbdi/microservicios/app/usuarios/repository/AlumnoRepository.java
+++ src/main/java/com/formacionbdi/microservicios/app/usuarios/repository/AlumnoRepository.java
@@ -1,5 +1,8 @@
 package com.formacionbdi.microservicios.app.usuarios.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +12,6 @@ import com.formacionbdi.microservicios.commons.alumnos.models.entity.Alumno;
 @Repository
 public interface AlumnoRepository extends CrudRepository<Alumno, Long> {
 
+	@Query("select a from Alumno a where a.nombre like %?1% or a.apellido like %?1%")
+	public List<Alumno> findByNombreOrApellido(String term);
 }
--- src/main/java/com/formacionbdi/microservicios/app/usuarios/service/AlumnoServiceImpl.java
+++ src/main/java/com/formacionbdi/microservicios/app/usuarios/service/AlumnoServiceImpl.java
@@ -1,6 +1,9 @@
 package com.formacionbdi.microservicios.app.usuarios.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.formacionbdi.microservicios.app.usuarios.repository.AlumnoRepository;
 import com.formacionbdi.microservicios.commons.alumnos.models.entity.Alumno;
@@ -8,6 +11,12 @@ import com.formacionbdi.microservicios.commons.service.CommonServiceImpl;
 
 @Service
 public class AlumnoServiceImpl extends CommonServiceImpl<Alumno, AlumnoRepository> implements AlumnoService {
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Alumno> findByNombreOrApellido(String term) {
+		return repository.findByNombreOrApellido(term);
+	}
 	
 
 }
